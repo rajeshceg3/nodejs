9c03f33b66dc5956b555d7e9e4637b8bd81acfa2
Rajesh <rajeshceg3@gmail.com>
2022-06-06 20:16:31 +0530

    http server

---
+++ http_server/server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+let PORT = process.argv[2] | 8080
+
+const httpserver = http.createServer()
+httpserver.listen(PORT, ()=> console.log(`Server listening on ${PORT}`))
+
+httpserver.on("request", (req, res)=>{
+    res.writeHead(200, "ok", {"Content-Type": "text/plain"})
+})
+
+httpserver.on("connection", ()=> console.log(" Client is connected"))
