b2e3b672f1119e54b3686a785b8f36aa61c8f7c3
Rajesh <rajeshceg3@gmail.com>
2022-06-07 15:21:23 +0000

    SSE

--- server_sent_events/server.js
+++ server_sent_events/server.js
@@ -5,18 +5,18 @@ app.get("/", ( req, res )=>{
 } )
 
 app.get("/stream", ( req, res ) =>{
-    res.setHeader({
-        "Content-Type": "text/event-stream"
-    })
-    periodic_fn(res)
+    res.setHeader(
+        "Content-Type", "text/event-stream"
+    )
+    periodic_fn(res);
 })
 
 const port = process.env.PORT || 8080
 const server = process.env.SERVER_NAME || "admin"
 
 function periodic_fn(res){
-    res.write(`SSE Event - ${Date.now()}`)
-    setTimeout( periodic_fn, 3000)
+    res.send(`SSE Event - ${Date.now()}`)
+    setTimeout( periodic_fn(res), 3000)
 }
 
 app.listen(port, ()=> console.log(`Server listening on ${port}`))
